0780cdbddf6ab02401740a27600072a441e13dc8
Chuanzhou Su <2523901380@qq.com>
2026-06-25 21:15:46 +0800

    商店界面适配二次确认框

--- Assets/Scripts/Module/Shop/ShopController.cs
+++ Assets/Scripts/Module/Shop/ShopController.cs
@@ -1,4 +1,5 @@
 using Common.Defines;
+using Module.Confirm;
 using MVC;
 using MVC.Controller;
 using MVC.View;
@@ -28,9 +29,10 @@ namespace Module.Shop
         public override void InitModuleEvent()
         {
             RegisterFunc("OpenShopView", OnOpenShopView);
-            RegisterFunc("Shop.Refresh", OnRefresh);
-            RegisterFunc("Shop.Recycle", OnRecycle);
+            RegisterFunc("Shop.Refresh",  OnRefresh);
+            RegisterFunc("Shop.Recycle",  OnRecycle);
             RegisterFunc("Shop.Continue", OnContinue);
+            RegisterFunc("Shop.BuyItem",  OnBuyItem);
         }
 
         private void OnOpenShopView(object[] args)
@@ -53,6 +55,38 @@ namespace Module.Shop
         private void OnRecycle(object[] args) { }
         private void OnContinue(object[] args) { }
 
+        private void OnBuyItem(object[] args)
+        {
+            if (args == null || args.Length == 0 || args[0] is not ShopSlotData slotData) return;
+
+            if (_shopModel.Gold < slotData.price)
+            {
+                ConfirmController.Show(new ConfirmModel
+                {
+                    mode = ConfirmModel.Mode.ConfirmOnly,
+                    title = "金币不足",
+                    message = $"购买「{slotData.name}」需要 {slotData.price} 金币\n当前金币 {_shopModel.Gold}，差 {slotData.price - _shopModel.Gold} 枚。",
+                    confirmText = "知道了"
+                });
+                return;
+            }
+
+            ConfirmController.Show(new ConfirmModel
+            {
+                mode = ConfirmModel.Mode.ConfirmCancel,
+                title = "确认购买",
+                message = $"购买「{slotData.name}」\n花费 {slotData.price} 金币，剩余 {_shopModel.Gold - slotData.price} 金币。",
+                confirmText = "购买",
+                cancelText = "取消",
+                onConfirm = () =>
+                {
+                    _shopModel.SetGold(_shopModel.Gold - slotData.price);
+                    slotData.isPurchased = true;
+                    RefreshView();
+                }
+            });
+        }
+
         private void RefreshView()
         {
             var view = GameApp.ViewManager.GetView((int)ViewType.ShopView);
@@ -61,3 +95,4 @@ namespace Module.Shop
         }
     }
 }
+
