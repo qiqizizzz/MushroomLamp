2f03999213d74d37f3d889289ee1c48f4b641ee3
Chuanzhou Su <2523901380@qq.com>
2026-07-04 13:06:13 +0800

    扑克牌音效加入

--- Assets/Scripts/Module/Blackjack/BlackjackView.cs
+++ Assets/Scripts/Module/Blackjack/BlackjackView.cs
@@ -17,6 +17,8 @@ namespace Module.Blackjack
 {
     public class BlackjackView : BaseView
     {
+        private const string SfxItemClick = "sfx_poker_card_flipping";
+
         private readonly List<Button> _itemButtons = new();
         private readonly List<CardSlot> _smallCards = new();
 
@@ -115,7 +117,11 @@ namespace Module.Blackjack
                 Button btn = _itemButtons[i];
                 if (btn == null) continue;
                 btn.onClick.RemoveAllListeners();
-                btn.onClick.AddListener(() => ApplyFunc(EventDefines.BlackjackDraw, itemIndex));
+                btn.onClick.AddListener(() =>
+                {
+                    GameApp.SoundManager?.PlayEffect(SfxItemClick);
+                    ApplyFunc(EventDefines.BlackjackDraw, itemIndex);
+                });
             }
         }
 
